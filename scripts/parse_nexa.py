5e362f74fd0a56b4c8ff28b035c6e77d3428a014
Ivar Holmqvist <ivarholmqvist@gmail.com>
2014-01-25 11:46:02 +0100

    Add DIM and PAUSE parsing.  - Add support for parsing extra data after ID{26}GOUUBB, according to the internets this is the DIM level.  - Also check for the PAUSE sequence.

--- scripts/parse_nexa.py
+++ scripts/parse_nexa.py
@@ -46,26 +46,36 @@ def parse_csv(fname):
         prev = sample
         seq.append((length_T, LOW if sample.val else HIGH))
 
+    seq.append((40, LOW)) # Fake pause @ end since there is not transition back to HIGH for the
+                          # last LOW
     return seq
 
 #States
 SCAN=0
 ID=1
 def parse_bit_single(seq, index):
-    s0 = seq[index]
-    s1 = seq[index+1]
+    try:
+        s0 = seq[index]
+        s1 = seq[index+1]
+    except IndexError:
+        raise EncodingError("End of sequence len:%d i:%d" % (len(seq), index))
+
     if s0 == (1, HIGH) and s1 == (1, LOW):
         return 1
     if s0 == (1, HIGH) and s1 == (5, LOW):
         return 0
-    raise Exception("PARSE BIT i:%d %d:%d %d:%d" % (index,s0[0],s0[1],s1[0],s1[1]))
+    raise EncodingError("PARSE BIT i:%d %d:%d %d:%d" % (index,s0[0],s0[1],s1[0],s1[1]))
 
 def parse_bit(seq, index):
     b0 = parse_bit_single(seq, index)
     b1 = parse_bit_single(seq, index + 2)
     if b0 == b1:
-        raise Exception("%d==%d" % (b0, b1))
+        raise EncodingError("i:%d %d==%d" % (i, b0, b1))
     return b0
+
+class EncodingError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
 
 def parse_bits(seq, index, bits):
     ret = 0
@@ -97,8 +107,31 @@ def parse_nexa(seq):
             (onoff, i)    = parse_bits(seq, i, 1)
             (unit, i)     = parse_bits(seq, i, 2)
             (button, i)   = parse_bits(seq, i, 2)
-            # FIXME: Parse single bit 1 + 40T LOW here
-            print "DONE: % -20s id:0x%x g:%d o:%d u:%d b:%d" % (os.path.basename(sys.argv[1]), trans_id, group, onoff, unit, button)
+            extra_data = ""
+
+            # Keep on parsing bits until it fails (which should be
+            # due to PAUSE bit)
+            # Note, this is not tested with _real_ data since
+            # I don't have a dimmer enabled remote.
+            while True:
+                print "ex:<%s>" % extra_data
+                try:
+                    (b, i) = parse_bits(seq, i, 1)
+                    extra_data = extra_data + "1" if b == HIGH else "0"
+                except EncodingError:
+                    break
+
+            # Now this MUST be the PAUSE bit [HIGH 1T, LOW ~40T (10ms)]
+            if seq[i] != (HIGH, 1) or seq[i+1][0] < 35 or seq[i+1][1] != LOW:
+                raise EncodingError("PAUSE not FOUND @%d %s" % (i, seq[i:i+2]))
+            else:
+                print "FOUND PAUSE %d @%d" % (seq[i+1][0], i)
+                i += 2
+
+
+            print "DONE: % -20s id:0x%x g:%d o:%d u:%d b:%d extra:<%s>" % (os.path.basename(sys.argv[1]),
+                                                                           trans_id, group, onoff, unit,
+                                                                           button, extra_data)
             state = SCAN
             continue
 
